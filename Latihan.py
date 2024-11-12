c5d954eebdca6282fdb0d746bef928e95c916c20
denta666 <fantasyjr6@gmail.com>
2024-11-12 19:23:45 +0700

    Add files via upload

---
+++ Latihan.py
@@ -0,0 +1,19 @@
+# nama
+# umur
+# pekerjaan ortu
+# gaji ortu
+# IPK
+
+
+
+
+
+nama = input('masukan nama:')
+umur = int (input('Masukan umur:'))
+pekerjaan_ortu = input('masukan pekerjaan_ortu:')
+gaji_ortu = int (input('masukan gaji ortu:'))
+ipk = (input('masukan ipk:'))
+pengecualian = ['dokter','tni','pns']
+
+
+hasil = 'boleh menerima beasiswa jika' if (umur >= 19) (gaji_ortu <= 1000000 ) else 'tidak boleh menerima beasiswa'
